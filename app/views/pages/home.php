a4c0913f49f73e7bc25d24f4cb7eb18405e1d2a5
brocorp <46473786+brocorp-dev@users.noreply.github.com>
2021-12-14 10:01:47 +0400

    bugfix

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -4,7 +4,7 @@
  
   <div class="max-w-7xl mx-auto py-16 px-4 lg:py-44 sm:px-6 lg:px-8">
     <div class="text-center">
-      <h2 class="text-2xl font-semibold text-red-600 tracking-wide uppercase"><?php echo($title) ?></h2>
+      <h2 class="text-2xl font-semibold text-green-600 tracking-wide uppercase"><?php echo($title) ?></h2>
       <p class="mt-1 text-5xl font-extrabold text-indigo-900 sm:tracking-tight lg:text-7xl"><?php echo($subtitle) ?></p>
     </div>
 
